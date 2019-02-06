5f6bde70d028582f757b0b45e7d1bce190f1c400
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-06 11:16:32 +0300

    the controller comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'comments/index'
+  get 'comments/new'
+  get 'comments/show'
+  get 'comments/create'
+  get 'comments/edit'
+  get 'comments/update'
+  get 'comments/destroy'
   get 'users/index'
   get 'users/new'
   get 'users/show'
