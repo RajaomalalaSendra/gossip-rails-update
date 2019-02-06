879f36aeca4152705aed4ef6982275eec7634915
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-06 10:42:45 +0300

    creation of controller static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/team'
+  get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
