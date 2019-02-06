76de19c213bd2adc58e9ba953af1e1886f12e02d
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-07 00:44:06 +0300

    new, show and comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Gossip.new(content: params[:content], user_id: 1 + rand(User.all.length), gossip_id: params[:id])
+    @comment = Comment.new(content: params[:content], user_id: 1 + rand(User.all.length), gossip_id: params[:gossip_id])
     if @comment.save
       redirect_to gossips_path
     else
@@ -19,11 +19,21 @@ class CommentsController < ApplicationController
   end
 
   def edit
+    @gossip = Comment.find(params[:gossip_id])
   end
 
   def update
+   @comment = Comment.new
+    if @comment.update(content: params[:content], user_id: 1 + rand(User.all.length), gossip_id: params[:gossip_id])
+      redirect_to gossips_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @gossip = Comment.find(params[:id])
+    @gossip.destroy
+    redirect_to gossips_path
   end
 end
