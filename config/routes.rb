9d4d906eda86059a414e01f4e1d32d58f2feeeb0
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-06 11:11:22 +0300

    controller gossips

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'gossips/index'
+  get 'gossips/new'
+  get 'gossips/show'
+  get 'gossips/create'
+  get 'gossips/edit'
+  get 'gossips/update'
+  get 'gossips/destroy'
+  get 'gossip/index'
+  get 'gossip/new'
+  get 'gossip/show'
+  get 'gossip/create'
+  get 'gossip/edit'
+  get 'gossip/update'
+  get 'gossip/destroy'
   get 'static_pages/team'
   get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
