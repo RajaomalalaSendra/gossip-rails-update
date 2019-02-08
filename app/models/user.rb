d5d955793c34a1fe919f087511f366d8e09d3029
RajaomalalaSendra <rajaomalalasendrampanjato@gmail.com>
2019-02-08 09:57:32 +0300

    modification user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	has_secure_password
+	has_secure_password 
 	has_many :gossips
 	belongs_to :city
 	has_many :comments
@@ -7,4 +7,5 @@ class User < ApplicationRecord
 	validates :email, uniqueness: true, presence: true
 	validates :first_name, presence: true
 	validates :last_name, presence: true
+	validates :password, presence: true, length: { minimum: 6 }
 end
